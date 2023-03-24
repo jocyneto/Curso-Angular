1b209545815560fa1a14a7567f4f1ea1dc71f014
Jocy Gonçalves Neto - PC <jocygneto@gmail.com>
2023-03-24 09:08:05 -0300

    adicionando componente manual e automatico no projeto

---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'meu-primeiro-component',
+  template:  `<p>Meu primeiro component criado manualmente!</p>`
+})
+export class MeuPrimeiroComponent {
+
+}
