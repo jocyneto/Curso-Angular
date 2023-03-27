b60cea48edd49ae8a00ba97649658989346fbf37
Jocy Gonçalves Neto - PC <jocygneto@gmail.com>
2023-03-27 10:15:21 -0300

    Separando o acesso da lista no service do componente

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 
+import { CursosService } from './cursos.service';
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
   styleUrls: ['./cursos.component.css']
 })
 export class CursosComponent {
-  cursos: string[] = ['Java', 'BI', 'C#']
+  cursos: string[];
 
-  constructor(){
-
+  constructor(private cursoService: CursosService){
+    this.cursos = cursoService.getCursos();
   }
 }
--- src/app/cursos/cursos.module.ts
+++ src/app/cursos/cursos.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { CursosComponent } from './cursos.component';
 import { CursosDetalhesComponent } from './cursos-detalhes/cursos-detalhes.component';
+import { CursosService } from './cursos.service';
 
 
 
@@ -17,6 +18,9 @@ import { CursosDetalhesComponent } from './cursos-detalhes/cursos-detalhes.compo
   exports:[
     CursosComponent
     ,CursosDetalhesComponent
+  ],
+  providers:[
+    CursosService
   ]
 })
 export class CursosModule { }
